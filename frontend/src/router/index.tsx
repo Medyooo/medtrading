7d430dfadf714f3dbf906d8532128abbbd7d7c2d
Medyooo <arbielbaidi6@gmail.com>
2026-04-19 11:59:18 +0200

    feat: implement protected routes
    - Add ProtectedRoute component
    - Redirect to /login if not authenticated
    - Add /dashboard route protected
    - Redirect / to /login
    
    Closes #6

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,9 +1,18 @@
 import { createBrowserRouter } from "react-router-dom"
 import LoginPage from "@/pages/auth/LoginPage"
 import RegisterPage from "@/pages/auth/RegisterPage"
+import ProtectedRoute from "@/router/ProtectedRoute.tsx";
 
 export const router = createBrowserRouter([
     { path: "/login", element: <LoginPage /> },
     { path: "/register", element: <RegisterPage /> },
     { path: "/", element: <LoginPage /> },
+    {
+        path: "/dashboard",
+        element: (
+            <ProtectedRoute>
+                <div>Dashboard — coming soon</div>
+            </ProtectedRoute>
+        ),
+    },
 ])
